1e1902cfa41deb7cf147e92813379ac1712b8450
aliaksei-barouka <borovko.reshaet@gmail.com>
2023-09-14 04:48:13 +0200

    Update all cases in main.spec.js Add several methods to Basket.js Add checks for methods in home.page.js Delete backet.spec.js

--- modules/Basket.js
+++ modules/Basket.js
@@ -7,17 +7,20 @@ class Basket {
         // locators
         this.basketDropdownButton = "//a[@id='dropdownBasket']";
         this.basketCounter = "//span[@class='basket-count-items badge badge-primary']";
-        this.basketItems = "//li[contains(@class, 'basket-item')]";
-        this.basketItemsPrice = "//span[@class='basket_price']";
-        this.redirectToBasket = "btn btn-primary btn-sm ml-auto"
+        this.basketDropDownItems = ".basket-item.list-group-item"
+        this.basketItemsName = "//span[@class='basket-item-title']";
+        this.basketItemsPrice = "//span[@class='basket-item-price']";
+        this.basketTotalPrice = "//span[@class='basket_price']";
+        this.redirectToBasketButton = "//a[@class='btn btn-primary btn-sm ml-auto']"
         this.clearItemsButton = "//a[@class='btn btn-danger btn-sm mr-auto']";
 
     }
 
-    async openBasketDropdownButton() {
+    async openBasketDropdown() {
         const basketDropdownElement = this.page.locator(this.basketDropdownButton);
         await basketDropdownElement.click();
-        await expect(basketDropdownElement).toHaveClass('dropdown-menu dropdown-menu-right show');
+        const ariaExpanded = await basketDropdownElement.getAttribute('aria-expanded');
+        await expect(ariaExpanded).toBe('true');
     }
 
     async triggerClearItemsButton() {
@@ -29,14 +32,56 @@ class Basket {
         return await this.page.locator(this.basketCounter).textContent();
     }
 
+    async redirectToBasket() {
+        const redirectToBasket = this.page.locator(this.redirectToBasketButton);
+        await redirectToBasket.click();
+    }
+
     async checkBasketCounterValueIsEmpty() {
         const basketCounterValue = await this.checkBasketCounterValue();
         if (basketCounterValue === '0') {
         } else {
-            await this.openBasketDropdownButton();
+            await this.openBasketDropdown();
             await this.triggerClearItemsButton();
         }
     }
+
+    async findDropDownItemsName() {
+        const findDropDownItemsName = await this.page.locator(this.basketDropDownItems).locator(this.basketItemsName);
+        await findDropDownItemsName.textContent();
+    }
+
+    async findDropDownItemsPrice() {
+        const findDropDownItemsPrice = await this.page.locator(this.basketDropDownItems).locator(this.basketItemsPrice);
+        await findDropDownItemsPrice.textContent();
+    }
+
+    async findTotalPrice() {
+        const findTotalPrice = await this.page.locator(this.basketTotalPrice);
+        await findTotalPrice.textContent();
+    }
+
+    async checkDropDownItemsHaveAllAttributes() {
+        await this.findDropDownItemsName();
+        await this.findDropDownItemsPrice();
+        await this.findTotalPrice();
+        await expect(this.findDropDownItemsName()).toBeTruthy && await expect(this.findDropDownItemsPrice()).toBeTruthy
+        && await expect(this.findTotalPrice()).toBeTruthy
+    }
+
+    async checkAllDropDownItemsHaveAllAttributes() {
+        await this.findDropDownItemsName();
+        await this.findDropDownItemsPrice();
+        await this.findTotalPrice();
+        for (let i = 0; i < this.findDropDownItemsName().length; i++) {
+            await expect(this.findDropDownItemsName().nth(i)).toBeTruthy() && await expect(this.findDropDownItemsName().nth(i)).toBeTruthy();
+        }
+        for (let i = 0; i < this.findDropDownItemsPrice().length; i++) {
+            await expect(this.findDropDownItemsPrice().nth(i)).toBeTruthy() && await expect(this.findDropDownItemsPrice().nth(i)).toBeTruthy();
+        }
+        await expect(this.findTotalPrice()).toBeTruthy();
+    }
+
 
 }
 
